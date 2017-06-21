fb487ddf8bae6b67d38a2fdac30666d5c779cad8
Lawrence Wolf-Sonkin <lawrence@duolingo.com>
2017-06-21 11:58:25 -0400

    Added a file system DAL for zendesk tickets, defaulting to marking all tickets as bugs

--- jeeves/dal/support_tickets.py
+++ jeeves/dal/support_tickets.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python2
 import json
 from itertools import imap
+from glob import glob
+import operator
 
 from jeeves.model.support_ticket import SupportTicket
 
@@ -7,22 +10,52 @@ class AbstractSupportTicketDAL(object):
     def get_support_tickets(self):
         pass
 
-class FileSystemSupportTicketDAL(AbstractSupportTicketDAL):
+    @staticmethod
+    def support_ticket_maker(category_label_func):
+        def factory(ticket_json):
+            return SupportTicket(
+                ticket_id=ticket_json['id'],
+                date_time=ticket_json['created_at'],
+                subject=ticket_json['subject'],
+                description=ticket_json['description'],
+                category_labels=category_label_func(ticket_json)
+            )
+        return factory
 
-    _file = 'data/category_dataset-en.txt'
+class AbstractFileSystemSupportTicketDAL(AbstractSupportTicketDAL):
+
+    def _ticket_generator(self, factory):
+        pass
+
+    def _category_label_func(self, ticket_json):
+        pass
 
     def get_support_tickets(self):
+        return self._ticket_generator(self.support_ticket_maker(self._category_label_func))
+
+class FileSystemSupportTicketDAL(AbstractFileSystemSupportTicketDAL):
+
+    _file = 'data/category_dataset-en.txt'
+    _category_label_func = staticmethod(operator.itemgetter('category_labels'))
+
+    def _ticket_generator(self, factory):
         with open(self._file, 'r') as input_file:
-            return [
-                SupportTicket(
-                    ticket_id=ticket_json['id'],
-                    date_time=ticket_json['created_at'],
-                    subject=ticket_json['subject'],
-                    description=ticket_json['description'],
-                    category_labels=ticket_json['category_labels']
-                )
-                for ticket_json in imap(json.loads, input_file)
-            ]
+            for ticket_json in imap(json.loads, input_file):
+                yield factory(ticket_json)
+
+class ZendeskFileSystemSupportTicketDAL(AbstractFileSystemSupportTicketDAL):
+
+    _category_label_func = staticmethod(lambda tk: ['bug'])
+
+    def __init__(self, directory):
+        super(ZendeskFileSystemSupportTicketDAL, self).__init__()
+        self._files = glob(directory + '/tickets_*.json')
+
+    def _ticket_generator(self, factory):
+        for fileName in self._files:
+            with open(fileName, 'r') as input_file:
+                for ticket_json in json.load(input_file)['tickets']:
+                    yield factory(ticket_json)
 
 
 SupportTicketDAL = FileSystemSupportTicketDAL()
