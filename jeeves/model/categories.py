5a0b449576f45d10d707f8ef5cb9dd196f1bffa3
Hideki Shima <hideki@duolingo.com>
2017-06-27 21:52:22 -0400

    Add more category ideas in comment

--- jeeves/model/categories.py
+++ jeeves/model/categories.py
@@ -1,5 +1,49 @@
 """
 A ticket can be assigned with multiple categories defined in this class.
+
+TODO: Support more categories.
+
+Lauren's spreadsheet has the following categoies:
+ - Lesson freezing
+ - Offline mode not working
+ - No sound
+ - "Could not connect to Duolingo"
+ - Windows issue
+ - Lost progress
+ - Strengthen skills
+ - Speaking challenge
+ - Android update
+ - iOS update
+ - Images not appearing
+ - Can’t access account
+ - Audio
+ - App is slow
+ - App crashes
+ - Fluency score
+ - Can't access skill
+ - Grading incorrectly
+ - Streak loss
+ - TTS
+ - Unable to log in
+ - Error
+ - Schools
+ - Progress quiz
+ - Issue at end of lesson
+ - Problem testing out
+ - Can't download app
+ - Can't access app
+ - Progress is wrong
+ - Notifications
+ - Streak payment
+ - Listen challenges
+ - Timed practice
+ - Other
+ - Chatbot
+ - Clubs
+ - Bonus skills
+ - Streak freeze
+ - Can't join clubs
+ - Clubs stream not showing
 """
 
 CATEGORIES = frozenset({'bug', 'suggestion', 'inappropriate_ad'})
