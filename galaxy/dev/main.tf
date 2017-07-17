010ca4496a12223c50827bb3e52f47d164dcb5d5
Hideki Shima <hidekishima@users.noreply.github.com>
2017-07-17 14:02:32 -0400

    Increase dev memory from 128MB to 256MB (#57)

--- galaxy/dev/main.tf
+++ galaxy/dev/main.tf
@@ -37,6 +37,7 @@ module "duolingo-jeeves" {
   subservice     = "api"
   min_count      = 1                                                           # Minimum number of tasks to run in autoscaling group
   max_count      = 1                                                           # Maximum number of tasks to run in autoscaling group
+  memory         = 256                                                         # 256MB memory (default: 128MB)
   product        = "${var.product}"
   owner          = "${var.owner}"                                              # The name of the owner for this service
   ecs_cluster    = "${var.ecs_cluster}"                                        # Name of the ECS cluster to run on
