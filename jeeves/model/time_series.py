b8281840fc5e717e1e56e7474d6bb53471dc394e
lwolfsonkin <lawrencews@jhu.edu>
2017-08-01 19:27:08 -0400

    More memory-conservative construction of time series dataframe (#72)
    * Should now be more resilient to Zendesk back-updating ancient tix
        * Will not load old tickets into dataframe in the first place
    * Fixed bug in construction of metadata rows of dataframe

--- jeeves/model/time_series.py
+++ jeeves/model/time_series.py
@@ -8,6 +8,7 @@ class TimeSeries(object):
 
     def __init__(self):
         self.__df = None
+        self.JAN_FIRST = pd.Timestamp('2017-01-01', tz='UTC')
 
     @property
     def df(self):
@@ -17,27 +18,33 @@ class TimeSeries(object):
 
     def __init_df(self):
         _df = pd.DataFrame()
-        _df['tickets'] = pd.Series(st for st in SupportTicketDAL.get_labeled_support_tickets())
+
+        _df['tickets'] = pd.Series(
+            st
+            for st in SupportTicketDAL.get_labeled_support_tickets()
+            if pd.Timestamp(st.date_time) > self.JAN_FIRST
+        )
+        print('predrop', _df.shape)
         _df['id'] = _df['tickets'].map(operator.attrgetter('ticket_id'))
         _df.drop_duplicates(subset='id', inplace=True)
         del _df['id']
 
         print('main', _df.shape)
 
-        _metadf = pd.DataFrame.from_records(dict(zip(STATS_FIELD_TITLES, operator.attrgetter(*STATS_FIELD_TITLES)(tk.metadata))) for tk in _df['tickets'])
-        _metadf = _metadf.apply(pd.Categorical)
-
-        print('meta', _metadf.shape)
-
-        _df = _df.join(_metadf)
-
-        print('joined', _metadf.shape)
-
         _df.set_index(
-            _df['tickets'].apply(lambda tk: pd.Timestamp(tk.date_time)),
+            _df['tickets'].map(lambda tk: pd.Timestamp(tk.date_time)),
             inplace=True
         )
         _df.sort_index(inplace=True)
+
+        print('set and sorted index')
+
+        for field in STATS_FIELD_TITLES:
+            # pylint: disable=W0640
+            _df[field] = _df['tickets'].map(lambda tk: getattr(tk.metadata, field)).astype('category', copy=False)
+
+        print('loaded metadata cols')
+
         self.__df = _df
 
 TS = TimeSeries()
