efafe63eaa99facf6affefe5432b87e87f8f4786
Hideki Shima <hidekishima@users.noreply.github.com>
2017-08-29 14:48:00 -0400

    Increase worker memory + add spike detection to worker task (#90)
    * Increase worker memory + add spike detection to worker task
    
    * Update s3-worker.json
    
    Remove double quotes
    
    * Increase memory from 800 to 1024MiB
    
    * Increase memory for dev settings too
    
    * Update memory size
    
    * Update memory for worker
    
    * dev->800. 512 causes OOM & 1024 is too large for t2.micro

--- galaxy/prod/main.tf
+++ galaxy/prod/main.tf
@@ -39,7 +39,7 @@ module "duolingo-jeeves" {
   min_count      = 1                                                           # Minimum number of tasks to run in autoscaling group
   max_count      = 5                                                           # Maximum number of tasks to run in autoscaling group
   scale_out_cpu  = 80                                                          # Scale out at this cpu usage (percent)
-  memory         = 800                                                         # Maximum memory (default: 128MB)
+  memory         = 1024                                                        # Maximum memory (default: 128MB)
   product        = "${var.product}"
   owner          = "${var.owner}"                                              # The name of the owner for this service
   ecs_cluster    = "${var.ecs_cluster}"                                        # Name of the ECS cluster to run on
@@ -54,7 +54,7 @@ module "duolingo-jeeves-s3-worker" {
   service                            = "${var.service}"
   subservice                         = "s3-worker"
   cpu                                = 1024
-  memory                             = 512
+  memory                             = 1024
   min_count                          = 1                                                              # Minimum number of tasks to run in autoscaling group
   max_count                          = 1                                                              # Maximum number of tasks to run in autoscaling group
   scale_out_count                    = 0
---
+++ jeeves/scripts/update_jeeves_data.sh
@@ -0,0 +1,5 @@
+# Download tickets
+python jeeves/scripts/update_s3_data.py
+
+# Detect spikes of keywords
+python jeeves/scripts/detect_spike.py
