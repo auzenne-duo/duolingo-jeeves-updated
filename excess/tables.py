b9f5ebc3cce2da9a56b42401c582a2ba2e206cdb
Hideki Shima <hidekishima@users.noreply.github.com>
2018-02-28 10:46:41 -0500

    Change Redshift table schema (#135)
    * Change Redshift table schema
    
    * Add user_id back, remove pk from created_at

--- excess/tables.py
+++ excess/tables.py
@@ -13,16 +13,17 @@ METADATA = MetaData(schema='duolingo_jeeves')
 Table for tickets exported from Zendesk.
 """
 ZENDESK_TICKET_TABLE = Table('zendesk_ticket', METADATA,
-    Column('user_id', BigInteger, info={'encode': 'LZO'}, primary_key=True),
-    Column('created_at', DateTime, info={'encode': 'LZO'}, primary_key=True),
+    Column('ticket_id', BigInteger, info={'encode': 'LZO'}, primary_key=True),
+    Column('created_at', DateTime, info={'encode': 'LZO'}),
 
     Column('subject', String(256), info={'encode': 'LZO'}),
     Column('description', String(16536), info={'encode': 'LZO'}),
 
+    Column('user_id', BigInteger, info={'encode': 'LZO'}),
     Column('category_labels', String(512), info={'encode': 'LZO'}),
     Column('metadata', String(16536), info={'encode': 'LZO'}),
 
     redshift_diststyle='KEY',
-    redshift_distkey='user_id',
+    redshift_distkey='ticket_id',
     redshift_sortkey='created_at',
 )
