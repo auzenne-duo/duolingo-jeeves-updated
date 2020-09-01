c715a6242b71f0f9f3d202737d7f6011bb4e8ecb
Aleksejs Popovs <aleksejs@duolingo.com>
2020-09-01 11:38:37 -0400

    Convert AWS IAM policies to Terraform syntax (OPS-16248) (#179)

--- galaxy/dev/s3.tf
+++ galaxy/dev/s3.tf
@@ -1,34 +1,28 @@
+data "aws_iam_policy_document" "s3-rw-duolingo-jeeves" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = ["arn:aws:s3:::duolingo-jeeves"]
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+    ]
+
+    resources = ["arn:aws:s3:::duolingo-jeeves/*"]
+  }
+}
+
 resource "aws_iam_role_policy" "s3-rw-duolingo-jeeves" {
   name = "s3-rw-duolingo-jeeves"
   role = "${module.duolingo-jeeves.iam_role}"
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetBucketLocation"
-      ],
-      "Resource": [
-        "arn:aws:s3:::duolingo-jeeves"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:PutObjectAcl",
-        "s3:GetObject",
-        "s3:GetObjectAcl"
-      ],
-      "Resource": [
-        "arn:aws:s3:::duolingo-jeeves/*"
-      ]
-    }
-  ]
-}
-POLICY
+  policy = "${data.aws_iam_policy_document.s3-rw-duolingo-jeeves.json}"
 }
--- galaxy/prod/s3.tf
+++ galaxy/prod/s3.tf
@@ -9,72 +9,38 @@ resource "aws_s3_bucket" "duolingo-jeeves" {
   }
 }
 
+data "aws_iam_policy_document" "s3-rw-duolingo-jeeves" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = ["arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}"]
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+    ]
+
+    resources = ["arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}/*"]
+  }
+}
+
 resource "aws_iam_role_policy" "s3-rw-duolingo-jeeves" {
   name = "s3-rw-duolingo-jeeves"
   role = "${module.duolingo-jeeves.iam_role}"
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetBucketLocation"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:PutObjectAcl",
-        "s3:GetObject",
-        "s3:GetObjectAcl"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}/*"
-      ]
-    }
-  ]
-}
-POLICY
+  policy = "${data.aws_iam_policy_document.s3-rw-duolingo-jeeves.json}"
 }
 
 resource "aws_iam_role_policy" "s3-rw-duolingo-jeeves-worker" {
   name = "s3-rw-duolingo-jeeves"
   role = "${module.duolingo-jeeves-s3-worker.iam_role}"
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetBucketLocation"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:PutObjectAcl",
-        "s3:GetObject",
-        "s3:GetObjectAcl"
-      ],
-      "Resource": [
-        "arn:aws:s3:::${aws_s3_bucket.duolingo-jeeves.bucket}/*"
-      ]
-    }
-  ]
-}
-POLICY
+  policy = "${data.aws_iam_policy_document.s3-rw-duolingo-jeeves.json}"
 }
