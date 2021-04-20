6a11c5830083f74b68675877ac5ccb79a3479e81
ppearsonDuolingo <ppearson@duolingo.com>
2021-04-20 15:53:42 -0400

    Revert "Revert "Enable encryption and block public access on private buckets (SEC-180)" (#307)" (#308)
    This reverts commit 9931603c8ade8fe3cf928e9e8d1a54ae5b0ada84.

--- galaxy/dev/s3.tf
+++ galaxy/dev/s3.tf
@@ -7,4 +7,21 @@ resource "aws_s3_bucket" "jeeves-document-cache-dev" {
     environment = var.environment
     owner       = var.owner
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "jeeves-document-cache-dev" {
+  bucket = aws_s3_bucket.jeeves-document-cache-dev.id
+
+  block_public_acls  = true
+  ignore_public_acls = true
+
+  block_public_policy = true
 }
