2c92d085755bc948b9888e08d301a448f0205dec
Rens Poesse <renspoesse@users.noreply.github.com>
2022-01-21 10:34:40 -0500

    Add fade effect to lightbox overlay (#379)

--- web/src/components/Lightbox.tsx
+++ web/src/components/Lightbox.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
-import { DimmedOverlay } from "web-ui";
+import { DimmedOverlay, Fade } from "web-ui";
 
 import AppStateContext from "contexts/AppStateContext";
 import styles from "styles/Lightbox.scss";
 
 const Lightbox = () => {
   const [state, dispatch] = React.useContext(AppStateContext);
-  return state.lightboxUrl ? (
-    <div className={styles.wrap} onClick={() => dispatch({ type: "ESCAPE" })}>
-      <DimmedOverlay />
-      <img alt="" className={styles.image} src={state.lightboxUrl} />
-    </div>
-  ) : null;
+  return (
+    <Fade isVisible={state.lightboxUrl !== undefined}>
+      <div className={styles.wrap} onClick={() => dispatch({ type: "ESCAPE" })}>
+        <DimmedOverlay />
+        <img
+          alt=""
+          className={styles.image}
+          key={state.lightboxUrl}
+          src={state.lightboxUrl}
+        />
+      </div>
+    </Fade>
+  );
 };
 
 export default Lightbox;
