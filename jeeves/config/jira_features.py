b66bfea10227f7375f8e852c99daad0cca81673b
ramya <56595004+Ramya1217@users.noreply.github.com>
2022-07-18 13:43:55 -0400

    Update jira_features.py (#469)
    * Update jira_features.py
    
    Adding a new feature for Retention related to "lesson complete"
    
    * Update jeeves/config/jira_features.py
    
    Co-authored-by: Antonia Scheidel <antonia@duolingo.com>
    
    Co-authored-by: Antonia Scheidel <antonia@duolingo.com>

--- jeeves/config/jira_features.py
+++ jeeves/config/jira_features.py
@@ -64,6 +64,7 @@ JIRA_FEATURES = {
             "Streak freeze / repair": ["streak freeze", "streak repair"],
             "Practice reminders / notifications": ["reminder", "notification"],
             "Early Bird / Night Owl Chests": ["early bird", "night owl"],
+            "Lesson complete": ["complete"],
         },
         "Time Spent Learning": {
             "Achievements": ["achievement"],
