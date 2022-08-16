c0b0dcaf6636eccc7bfc184b40d7873bb17ff6e2
AJ <andrew.guidetti@gmail.com>
2022-08-16 15:50:12 -0400

    Add internal access to Jeeves ElasticSearch prod cluster (#495)

--- galaxy/prod/elasticsearch.tf
+++ galaxy/prod/elasticsearch.tf
@@ -7,3 +7,16 @@ module "jeeves-elasticsearch" {
   owner                 = var.owner
   environment           = var.environment
 }
+
+data "aws_security_group" "duolingo-jeeves-es-prod-internal-esd" {
+  name = "duolingo-jeeves-es-prod-internal-esd"
+}
+
+resource "aws_security_group_rule" "allow-vpn-ingress-https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/8"]
+  security_group_id = data.aws_security_group.duolingo-jeeves-es-prod-internal-esd.id
+}
