d0e592d7626f006df51ae7ca2ef42e34cb876fd7
calebnoble <108286348+calebnoble@users.noreply.github.com>
2023-05-31 14:57:58 -0400

    Specify which workers to redeploy for dev (DEL-2568) (#716)

--- jenkins-build-dev.sh
+++ jenkins-build-dev.sh
@@ -30,13 +30,23 @@ IMAGE_HASH="$(docker build -q -f "$DOCKER_FILE" . | head -1)"
 
 # ----- deploy -----
 
-echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
-if $DEPLOY_ALL_WORKERS; then
+if $DEPLOY_API; then
+  echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
+fi
+if $DEPLOY_INDEX_PIPELINE; then
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_S3_WORKER" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
-  echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_WORKER_CRON" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_SQS_WORKER_1" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_SQS_WORKER_2" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
+fi
+if $DEPLOY_SPIKE_SLACK_WORKER; then
+  echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_WORKER_CRON" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
+fi
+if $DEPLOY_SPIKE_WORKER; then
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_SPIKE_WORKER" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
+fi
+if $DEPLOY_EMAIL_SENDER; then
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_EMAIL_SENDER" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
+fi
+if $DEPLOY_PRIORITY_ESTIMATOR_UPDATER; then
   echo "$IMAGE_HASH" | deploy-galaxy -c -m "$MODULE_PRIORITY_ESTIMATOR_UPDATER" -v "$BUILD_NUMBER" -p "$TERRAFORM_PATH"
 fi
