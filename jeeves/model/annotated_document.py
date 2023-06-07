9c1e66093adc7f1a61a46a8b93a23292ae98a779
shelbywengreen <107069453+shelbywengreen@users.noreply.github.com>
2023-06-07 13:27:28 -0400

    Add functions for visualizing dataset (TRI-3258) (#724)
    Co-authored-by: Shelby Wengreen <shelby@C-4028-macbook-pro.local>

--- jeeves/model/annotated_document.py
+++ jeeves/model/annotated_document.py
@@ -12,3 +12,9 @@ class AnnotatedDocument:
 
     jeeves_document: JeevesDocument = attr.ib()
     label: str = attr.ib()
+
+    def get_summary_string(self) -> str:
+        """
+        Returns a summary of the jeeves document and its label
+        """
+        return f"(document id: {self.jeeves_document.document_id}. label: {self.label}. header text: {self.jeeves_document.header_text}. body text: {self.jeeves_document.body_text})"
---
+++ jeeves/util/sentiment_dataset_summarizer.py
@@ -0,0 +1,23 @@
+"""
+A utility that summarizes an annotated dataset as a TSV file
+"""
+
+import csv
+from typing import List
+
+from jeeves.model.annotated_document import AnnotatedDocument
+
+
+def summarize_dataset(filename: str, labeled_data: List[AnnotatedDocument]) -> None:
+    """
+    Allow users to more easily view the annotated dataset by saving just the document
+    id, label, header, and body to a tsv file
+    """
+    with open(filename, "w", newline="") as tsvfile:
+        writer = csv.writer(tsvfile, delimiter="\t", lineterminator="\n")
+        writer.writerow(["doc_id", "label", "header", "body"])
+        for labeled_doc in labeled_data:
+            label = labeled_doc.label
+            document = labeled_doc.jeeves_document
+            print(document.document_id)
+            writer.writerow([document.document_id, label, document.header_text, document.body_text])
