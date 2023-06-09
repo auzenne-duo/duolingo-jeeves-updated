457273eedf0409323c44b2424186b0e0bce9a78b
shelbywengreen <107069453+shelbywengreen@users.noreply.github.com>
2023-06-09 08:41:16 -0400

    Add a function to evaluate model performance (TRI-3243) (#725)
    Co-authored-by: Shelby Wengreen <shelby@C-4028-macbook-pro.local>

--- jeeves/model/sentiment_analysis_classifier.py
+++ jeeves/model/sentiment_analysis_classifier.py
@@ -3,15 +3,48 @@ Our model for a general abstract sentiment analysis classifier
 """
 
 from abc import ABC, abstractmethod
-from typing import Tuple
+from ctypes import Array
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
 
 import attr
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+)
 
+from jeeves.model.annotated_document import AnnotatedDocument
 from jeeves.model.jeeves_document import JeevesDocument
 
 POSITIVE_CLASS = "positive"
 NEGATIVE_CLASS = "negative"
 NEUTRAL_CLASS = "none"
+
+
+@dataclass
+class SentimentLabelResults:
+    """
+    Class to hold metrics about a single label of a sentiment analysis classifier
+    """
+
+    precision: float
+    recall: float
+    f1_score: float
+
+
+@dataclass
+class SentimentClassifierResults:
+    """
+    Class to hold metrics about a sentiment analysis classifier
+    """
+
+    confusion_matrix: Array
+    accuracy: float
+    positive_metrics: SentimentLabelResults
+    negative_metrics: SentimentLabelResults
 
 
 @attr.s(kw_only=True)
@@ -27,3 +60,33 @@ class SentimentAnalysisClassifier(ABC):
         Takes in a JeevesDocument and returns the predicted sentiment label and the polarity
         """
         return NotImplementedError
+
+    def evaluate_classifier(self, labeled_data: List[AnnotatedDocument]) -> Dict:
+
+        gt = []
+        predicted = []
+        results = {}
+
+        labels_list = [self.positive_class, self.negative_class]
+
+        for labeled_document in labeled_data:
+            label = labeled_document.label
+            document = labeled_document.jeeves_document
+
+            if label in labels_list:
+                gt.append(label)
+                predicted.append(self.classify(document)[0])
+
+        for label in labels_list:
+            results[label] = SentimentLabelResults(
+                precision_score(gt, predicted, labels=[label], average=None)[0],
+                recall_score(gt, predicted, labels=[label], average=None)[0],
+                f1_score(gt, predicted, labels=[label], average=None)[0],
+            )
+
+        return SentimentClassifierResults(
+            confusion_matrix(gt, predicted, labels=labels_list),
+            accuracy_score(gt, predicted),
+            positive_metrics=(results[POSITIVE_CLASS]),
+            negative_metrics=(results[NEGATIVE_CLASS]),
+        )
