3ddfa384aa780585ee3186658c994116f0c5a6b0
Josh Abrams <josh@duolingo.com>
2023-11-17 12:16:23 -0500

    Add Rollbar for client errors (TRI-4032) (#919)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,7 @@ import "whatwg-fetch";
 import "normalize.css";
 import "web-ui/styles/fonts.scss";
 
+import { ErrorBoundary, Provider } from "@rollbar/react";
 import * as React from "react";
 import { render } from "react-dom";
 import {
@@ -13,18 +14,30 @@ import {
 } from "react-router-dom";
 
 import App from "components/App";
+
 import "images/favicon.ico";
 import "styles/index.scss";
 
+const rollbarConfig = {
+  accessToken: "58450d5ce0c24f97aa30aca8003ab95d",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: process.env.NODE_ENV,
+};
+
 const AppWithRouter = () => (
-  <Router>
-    <Switch>
-      <Redirect exact={true} from="/" to="/en" />
-      <Route path="/:lang">
-        <App />
-      </Route>
-    </Switch>
-  </Router>
+  <Provider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Router>
+        <Switch>
+          <Redirect exact={true} from="/" to="/en" />
+          <Route path="/:lang">
+            <App />
+          </Route>
+        </Switch>
+      </Router>
+    </ErrorBoundary>
+  </Provider>
 );
 
 render(<AppWithRouter />, document.getElementById("root"));
