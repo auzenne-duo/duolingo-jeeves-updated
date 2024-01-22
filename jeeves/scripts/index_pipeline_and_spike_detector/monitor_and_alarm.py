01eb38f3dd498f80caf37bdefb02eaca729b33c1
Aaron Wang <aaron.wang@duolingo.com>
2024-01-22 12:12:10 -0800

    Create monitoring and alerting script (TRI-3793) (#938)
    Co-authored-by: David Sawicki <david.sawicki@duolingo.com>

---
+++ jeeves/scripts/index_pipeline_and_spike_detector/monitor_and_alarm.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from datetime import datetime, timedelta, timezone
+
+import rollbar
+from duolingo_base.config import Config
+
+from jeeves import apply_registry, close_registry, registry as app_registry
+from jeeves.dal.opensearch_interface import OpenSearchDAL
+from jeeves.util.slack_util import SlackUtil
+
+_DEFAULT_SLACK_CHANNEL_ID = "C0UDM7XA4"  # for #slack-test channel
+_SLACK_CHANNEL_ID = os.environ.get("SLACK_CHANNEL_ID", _DEFAULT_SLACK_CHANNEL_ID)
+_SLACK_API_TOKEN = os.environ.get("SLACK_API_TOKEN")
+
+
+config = Config.load_config()
+config.apply_logging()
+config.apply_rollbar()
+
+
+DATA_SOURCES_MAX_EXPECTED_AGE = {
+    # Use larger stale threshold for AppFigures because we get them once for the whole day after they become available.
+    "AppFigures": timedelta(days=3),
+    "JIRA": timedelta(days=1),
+    "Reddit": timedelta(days=2),
+    "Zendesk": timedelta(days=2),
+}
+
+
+def get_latest_document(data_source: str) -> None:
+    """
+    Get the latest document from OpenSearch for the given data source.
+    """
+    query = {
+        "size": 1,
+        "sort": {"date_time": "desc"},
+        "query": {"term": {"data_source": data_source}},
+    }
+
+    indexed_issues = app_registry(OpenSearchDAL).execute_arbitrary_query(query)
+    return indexed_issues[0]
+
+
+if __name__ == "__main__":
+    apply_registry()
+    slack_obj = SlackUtil(slack_channel_id=_SLACK_CHANNEL_ID, slack_api_token=_SLACK_API_TOKEN)
+    try:
+        for data_source, max_expected_age in DATA_SOURCES_MAX_EXPECTED_AGE.items():
+            print(f"Checking {data_source}...")
+            doc = get_latest_document(data_source)
+            print(
+                f"Latest {data_source} document: {doc.jeeves_uid}; Creation time: {doc.date_time}"
+            )
+            print(
+                f"Difference between now and creation time: {datetime.now(timezone.utc) - doc.date_time}. Max expected age: {max_expected_age}"
+            )
+            if datetime.now(timezone.utc) - doc.date_time > max_expected_age:
+                msg = f"JEEVES WARNING: {data_source} data is stale! Latest document: {doc.jeeves_uid}; Creation time: {doc.date_time}."
+                if os.environ.get("BUILD_URL"):
+                    msg += f"See {os.environ.get('BUILD_URL')} for more info."
+                slack_obj.send_slack_message(msg)
+
+        print("Finished checking data sources.")
+    except:
+        print("Unexpected error:", sys.exc_info())
+        rollbar.report_exc_info(sys.exc_info())
+    finally:
+        close_registry()
---
+++ jeeves/util/slack_util.py
@@ -0,0 +1,45 @@
+import json
+
+from requests import post
+from requests.exceptions import RequestException
+
+from jeeves.util.error_util import print_request_exception
+
+
+class SlackUtil:
+    """
+    Utility class for sending messages to Slack.
+    """
+
+    def __init__(self, slack_channel_id: str, slack_api_token: str = None):
+        self.slack_channel_id = slack_channel_id
+        self.slack_api_token = slack_api_token
+
+    def send_slack_message(self, slack_message: str) -> None:
+        """
+        Given a message, send the message to the channel.
+        """
+        url = "https://slack.com/api/chat.postMessage"
+        headers = {
+            "Authorization": f"Bearer {self.slack_api_token}",
+            "Content-Type": "application/json; charset=utf-8",
+        }
+
+        block = {"text": {"text": slack_message, "type": "mrkdwn"}, "type": "section"}
+        data = {
+            "channel": self.slack_channel_id,
+            "blocks": [block],
+        }
+
+        print(f"Sending slack message to {self.slack_channel_id}: {slack_message}")
+
+        try:
+            r = post(url, headers=headers, data=json.dumps(data))
+            r.raise_for_status()
+            print(f"Slack POST response code: {r.status_code}. Response body: {r.json()}")
+
+            # It's lame that slack API returns status code 200 for errors like invalid auth token. See https://api.slack.com/methods/chat.postMessage#errors
+            if not r.json().get("ok", False):
+                raise RequestException(f"Slack POST failed with error: {r.json()['error']}")
+        except RequestException as e:
+            print_request_exception(e, rollbar_level="error")
