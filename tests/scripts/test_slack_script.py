8d0b1ab47848da25efbaacc64f1b88a8408771e3
shelbywengreen <107069453+shelbywengreen@users.noreply.github.com>
2024-02-14 09:32:33 -0500

    Add unit tests for slack bots (TRI-4378) (#967)
    Co-authored-by: Shelby Wengreen <shelby@C-5325-macbook-pro.local>

--- tests/scripts/test_slack_script.py
+++ tests/scripts/test_slack_script.py
@@ -1,11 +1,13 @@
 import unittest
 
+from jeeves.model.slack_bot import SlackBot
 from jeeves.model.spike_categories import SpikeCategory
 from jeeves.model.spike_word import SpikeWord
 from jeeves.scripts.index_pipeline_and_spike_detector.slack import (
     generate_slack_message,
     get_jeeves_analysis_query_params,
     get_yesterdays_date,
+    spike_sorter,
 )
 from jeeves.util.error_util import SpikeReporterException
 
@@ -158,4 +160,274 @@ class TestSlackScript(unittest.TestCase):
                     ],
                 },
             ],
+        )
+
+    def test_spike_sorter_for_customer_feedback(self):
+        """
+        Tests that when given a batch of customer feedback spikes the spike sorter correctly
+        divides spikes into bug spikes and social trend spikes and doesn't give any spikes
+        to the beta spike reporter
+        """
+        testObj1 = SpikeWord(
+            word="duo1",
+            score=1,
+            date="2022-01-01",
+            lang="en",
+            is_bug=True,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj2 = SpikeWord(
+            word="duo2",
+            score=2,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj3 = SpikeWord(
+            word="duo3",
+            score=3,
+            date="2022-01-01",
+            lang="en",
+            is_bug=True,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj4 = SpikeWord(
+            word="duo4",
+            score=4,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=True,
+            spike_group=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj5 = SpikeWord(
+            word="duo5",
+            score=5,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=True,
+            spike_group=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+            summary="test summary",
+        )
+
+        result = spike_sorter(
+            spikes=[testObj1, testObj2, testObj3, testObj4, testObj5],
+            curr_spike_category=SpikeCategory.EXTERNAL_NON_STR_SPIKES,
+        )
+
+        self.assertEqual(
+            set(result.keys()),
+            {
+                SlackBot.SPIKE_REPORTER.slack_name,
+                SlackBot.BUG_SPIKE_REPORTER.slack_name,
+                SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name,
+                SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name,
+            },
+        )
+
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SPIKE_REPORTER.slack_name],
+                    [testObj5, testObj4, testObj3, testObj2],
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BUG_SPIKE_REPORTER.slack_name], [testObj3, testObj1]
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name], [testObj5, testObj4]
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+
+    def test_spike_sorter_for_beta_feedback(self):
+        """
+        Tests that when given a batch of beta feedback spikes the spike sorter correctly
+        gives all the spikes to the beta spike reporter and doesn't give any spikes to
+        the bug spike reporter or social trend spike reporter
+        """
+        testObj1 = SpikeWord(
+            word="duo1",
+            score=1,
+            date="2022-01-01",
+            lang="en",
+            is_bug=True,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj2 = SpikeWord(
+            word="duo2",
+            score=2,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj3 = SpikeWord(
+            word="duo3",
+            score=3,
+            date="2022-01-01",
+            lang="en",
+            is_bug=True,
+            is_social_trend=False,
+            spike_group=SpikeCategory.EXTERNAL_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj4 = SpikeWord(
+            word="duo4",
+            score=4,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=True,
+            spike_group=SpikeCategory.EXTERNAL_STR_SPIKES,
+            summary="test summary",
+        )
+
+        testObj5 = SpikeWord(
+            word="duo5",
+            score=5,
+            date="2022-01-01",
+            lang="en",
+            is_bug=False,
+            is_social_trend=True,
+            spike_group=SpikeCategory.EXTERNAL_STR_SPIKES,
+            summary="test summary",
+        )
+
+        result = spike_sorter(
+            spikes=[testObj1, testObj2, testObj3, testObj4, testObj5],
+            curr_spike_category=SpikeCategory.EXTERNAL_STR_SPIKES,
+        )
+
+        self.assertEqual(
+            set(result.keys()),
+            {
+                SlackBot.SPIKE_REPORTER.slack_name,
+                SlackBot.BUG_SPIKE_REPORTER.slack_name,
+                SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name,
+                SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name,
+            },
+        )
+
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SPIKE_REPORTER.slack_name],
+                    [testObj5, testObj4, testObj3, testObj2],
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BUG_SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name],
+                    [testObj5, testObj4, testObj3, testObj2],
+                )
+            )
+        )
+
+    def test_spike_sorter_for_no_spikes(self):
+        """
+        Tests that the spike sorter doesn't throw an error when there are no spikes
+        """
+        result = spike_sorter(spikes=[], curr_spike_category=SpikeCategory.EXTERNAL_NON_STR_SPIKES)
+
+        self.assertEqual(
+            set(result.keys()),
+            {
+                SlackBot.SPIKE_REPORTER.slack_name,
+                SlackBot.BUG_SPIKE_REPORTER.slack_name,
+                SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name,
+                SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name,
+            },
+        )
+
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BUG_SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.SOCIAL_TRENDS_SPIKE_REPORTER.slack_name], []
+                )
+            )
+        )
+        self.assertTrue(
+            all(
+                resultSpike == expectedSpike
+                for resultSpike, expectedSpike in zip(
+                    result[SlackBot.BETA_FEEDBACK_SPIKE_REPORTER.slack_name], []
+                )
+            )
         )
