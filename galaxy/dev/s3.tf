96eff40657c2f67a84aa83530726dbdb408fa51a
Aaron Wang <aaron.wang@duolingo.com>
2024-10-22 12:46:24 -0700

    Add 1 year retention policy for dev (TRI-5204) (#1097)

--- galaxy/dev/s3.tf
+++ galaxy/dev/s3.tf
@@ -25,3 +25,59 @@ resource "aws_s3_bucket_public_access_block" "jeeves-document-cache-dev" {
 
   block_public_policy = true
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "jeeves-document-cache-dev-cleanup" {
+  bucket = aws_s3_bucket.jeeves-document-cache-dev.id
+  rule {
+    id = "jeeves-document-cache-dev-cleanup-app-figures"
+    filter {
+      prefix = "AppFigures/"
+    }
+    status = "Enabled"
+    expiration {
+      days = 365
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+  rule {
+    id = "jeeves-document-cache-dev-cleanup-zendesk"
+    filter {
+      prefix = "Zendesk/"
+    }
+    status = "Enabled"
+    expiration {
+      days = 365
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+  rule {
+    id = "jeeves-document-cache-dev-cleanup-jira"
+    filter {
+      prefix = "JIRA/"
+    }
+    status = "Enabled"
+    expiration {
+      days = 365
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+  rule {
+    id = "jeeves-document-cache-dev-cleanup-reddit"
+    filter {
+      prefix = "Reddit/"
+    }
+    status = "Enabled"
+    expiration {
+      days = 365
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
