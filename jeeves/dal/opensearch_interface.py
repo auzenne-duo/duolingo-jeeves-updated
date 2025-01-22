4593a22fd915ac30d2e885b8eb25e1ac5c472eb8
Nancy Zhang <nancy@duolingo.com>
2025-01-22 10:19:06 -0500

    Bump OpenSearch Fields Limit to 10000 (#1157)

--- jeeves/dal/opensearch_interface.py
+++ jeeves/dal/opensearch_interface.py
@@ -53,7 +53,7 @@ _config = get_config()
 LOG = logging.getLogger(__name__)
 
 # Default limit of 1000 must be increased to provide enough space for ~500 experiment conditions
-_MAX_FIELDS_LIMIT = 8000
+_MAX_FIELDS_LIMIT = 10000
 # If we ever change the duplicate detection model, make sure this value is
 # updated appropriately
 _SENTENCE_TRANSFORMERS_VECTOR_SIZE = 768
