6d3e2dbfe116df6d48afdc697e3595de234ac43a
Guanhua Li <106110284+Guanhua2@users.noreply.github.com>
2025-02-17 13:32:06 -0500

    Filter out localization contractors Jira tickets from quality report (#1170)

--- jeeves/config/config.py
+++ jeeves/config/config.py
@@ -18,6 +18,9 @@ VIDEO_CALL_PROJECTS = ["VCCF", "VCBF", "EXAI", "VCS", "VCG"]
 # Jira projects that are used for bug reporting
 JIRA_PROJECTS = ["DLAA", "DLAI", "DLAW"] + VIDEO_CALL_PROJECTS
 
+# Localization contractors label
+JIRA_LOCALIZATION_CONTRACTORS_LABEL = "localization-contractor"
+
 # Jira issue type label for bugs
 JIRA_ISSUE_TYPE_BUG = "Bug"
 
