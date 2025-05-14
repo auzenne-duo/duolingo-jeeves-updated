284eb5bd0011a2e0057e1d540d9e37dcffdeccea
cainan-winquist <cainan@duolingo.com>
2025-05-14 16:05:24 -0700

    Create a set of features that will be enabled for log summarization (#1244)
    ## Description
    
    This PR adds support for log summarization by introducing a new set of constants and collections to identify which features should have log summarization enabled.
    
    ## Verification steps
    
    - [x] Run tests to confirm that the enabled features are a subset of all features

--- tests/config/test_jira_features.py
+++ tests/config/test_jira_features.py
@@ -1,4 +1,8 @@
-from jeeves.config.jira_features import JIRA_FEATURES, JIRA_FEATURES_DESCRIPTIONS
+from jeeves.config.jira_features import (
+    JIRA_FEATURES,
+    JIRA_FEATURES_DESCRIPTIONS,
+    LOG_SUMMARIZATION_ENABLED_FEATURES,
+)
 
 
 def test_terms_are_disjoint():
@@ -7,7 +11,7 @@ def test_terms_are_disjoint():
     for pillar in JIRA_FEATURES:
         for teams_to_features in JIRA_FEATURES[pillar].values():
             for features_to_synonyms in teams_to_features.values():
-                for feature, synonyms in features_to_synonyms.items():
+                for feature, _ in features_to_synonyms.items():
                     assert feature.upper() not in terms
                     terms.add(feature.upper())
 
@@ -23,3 +27,19 @@ def test_descriptions_is_subset():
 
     for description_feature in JIRA_FEATURES_DESCRIPTIONS:
         assert description_feature.upper() in features
+
+
+def test_log_summarization_enabled_features_is_subset():
+    all_features = {
+        feature
+        for pillar_features in JIRA_FEATURES.values()
+        for area_features in pillar_features.values()
+        for team_features in area_features.values()
+        for feature in team_features
+    }
+    # All enabled features should be in the set of all features
+    assert LOG_SUMMARIZATION_ENABLED_FEATURES.issubset(all_features)
+    # Should not be empty
+    assert len(LOG_SUMMARIZATION_ENABLED_FEATURES) > 0
+    assert "Video Call" in LOG_SUMMARIZATION_ENABLED_FEATURES
+    assert "Streak" in LOG_SUMMARIZATION_ENABLED_FEATURES
